51eb47922cc858f521dea841ffa0766a52f32aff
Pabiak <pabiak17@gmail.com>
2023-05-05 22:02:27 +0200

    add prices section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import Header from './components/Header/header.component'
 import Info from './components/Info/Info.component'
+import Prices from './components/Prices/Prices.component'
 import Team from './components/Team/team.component'
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Info />
       <Team />
+      <Prices />
     </>
   )
 }
---
+++ src/components/Prices/Prices.component.jsx
@@ -0,0 +1,17 @@
+import './Prices.styles.scss'
+
+const Prices = () => {
+  return (
+    <div className="prices">
+      <div className="pricetext">
+        <span className="white">our&nbsp;</span>
+        <span className="orange">prices</span>
+      </div>
+      <div className="priceboard">
+        <img src="public/cennik.jpg" alt="cennik" />
+      </div>
+    </div>
+  )
+}
+
+export default Prices
