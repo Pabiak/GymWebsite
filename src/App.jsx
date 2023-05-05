deabe7b5bfd3a3e8ace44eddd29cbd790abd6246
Pabiak <pabiak17@gmail.com>
2023-05-05 21:13:26 +0200

    add info section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import Header from './components/Header/header.component'
+import Info from './components/Info/Info.component'
 import Team from './components/Team/team.component'
 const App = () => {
   return (
     <>
       <Header />
+      <Info />
       <Team />
     </>
   )
---
+++ src/components/Info/Info.component.jsx
@@ -0,0 +1,42 @@
+import './Info.styles.scss'
+
+const Info = () => {
+  return (
+    <div className="infowrapper">
+      <div className="flexwrapper">  {/*I kinda needed this, but couldn't came out with better name idea */}
+        <div className="info">
+          <div className="infotext">
+            <span className="white">welcome to&nbsp;</span>
+            <span className="orange">gympower</span>
+          </div>
+          <p className="lorem">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aut
+            accusamus unde voluptas numquam commodi nihil asperiores molestiae
+            assumenda dolor nemo temporibus
+          </p>{' '}
+          <p className="lorem">
+            rem maxime facere sequi voluptatum aperiam delectus voluptates velit
+            iure. Obcaecati, enim repellat! Sint non laborum totam? Doloremque
+            earum excepturi alias, u saepe enim accusamus
+          </p>
+          <p className="lorem">
+            {' '}
+            necessitatibus, quia porro. Eligendi neque explicabo numquam velit
+            quidem magni quibusdam officiis modi veniam rerum dolore ratione,
+            culpa accusantium 
+          </p>
+          <button className="readmore">read more</button>
+        </div>
+        <div className="location">
+          <div className="infotext">
+            <span className="white">where to find&nbsp;</span>
+            <span className="orange">us</span>
+          </div>
+          <img src="public/map.png" alt="map with gym location" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Info
